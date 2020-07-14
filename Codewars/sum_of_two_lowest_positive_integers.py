4c1488824f0269aa6dac720a7f1d898265d15d25
Ayşıl Simge Karacan <aysilsimgekaracan@hotmail.com>
2020-07-14 18:11:55 +0300

    7 kyu - Sum of two lowest positive integers

---
+++ Codewars/sum_of_two_lowest_positive_integers.py
@@ -0,0 +1,7 @@
+# 7 kyu
+# Sum of two lowest positive integers
+# https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python
+
+def sum_two_smallest_numbers(numbers):
+    numbers.sort()
+    return sum(numbers[:2])
